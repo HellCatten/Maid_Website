3586afbd405c8461c706cd54917b90b8314d06c5
HellCatten <basalykov.ivan@gmail.com>
2026-09-09 16:22:22 +0300

    discord url

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ export const typeColors = {
 
 export const HUB_URL = "https://hub.spacestation14.com/api/servers"; 
 export const TARGET_ADDRESS = "ss14://maidstation14.ru";
-export const DISCORD_URL = "https://discord.gg/your-link";
+export const DISCORD_URL = "https://discord.gg/QrWYVjWs9u";
 export const GITHUB_URL = "https://github.com/TheMaidDev";
 export const BOOSTY_URL = "https://boosty.to/aday";
 export const NEWS_CHANNEL = "https://discord.com/channels/1409197881163776064/1409231818544382012";
